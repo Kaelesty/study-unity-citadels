5ca1bc47fda87233fb1da38fdd1604e54fa417c2
Kaelesty <74826130+Kaelesty@users.noreply.github.com>
2023-05-21 11:59:12 +0300

    Тэги для экрана шпионажа

--- Scripts/Rework2/SpyScreenManager.cs
+++ Scripts/Rework2/SpyScreenManager.cs
@@ -76,12 +76,14 @@ public class SpyScreenManager : MonoBehaviour
 
         var cardChar = csm.InstantiateCharCard(presetChar);
         cardChar.transform.LeanMove(new Vector3(2000, 0, cardChar.transform.position.z), 1);
+        cardChar.tag = "SpyMenuCharacter";
 
         for (int i = 0; i < target.buildedDistricts.Count; i++)
         {
             presetDist = target.buildedDistricts[i];
             var cardDist = csm.InstantiateDistrictCard(presetDist);
             cardDist.transform.LeanMove(new Vector3(1800 + i*shift, 1150, cardChar.transform.position.z), 1);
+            cardDist.tag = "SpyMenuDistricts";
         }
         // TODO : Экран шпионажа
         // В переменной targetNickname лежит НИКНЕЙМ игрока, за которым хотим шпионить
